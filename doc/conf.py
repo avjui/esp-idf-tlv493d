59dbde5b2176bdb0ac00671606a96f73ec947a7c
avjui <jrene6774@gmail.com>
2025-01-11 21:18:08 +0100

    fix(conf.py): change version to semver style

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ project = 'ESP-IDF-TLV493D'
 copyright = '2025, Juen Rene'
 author = 'Juen Rene'
 
-version = "0.1"
+version = "0.1.0"
 
 root_doc = 'index'
 # -- General configuration ---------------------------------------------------
